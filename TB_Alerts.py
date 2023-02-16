4c444b801518a4ce71144c532e52f66b351496c4
Tanner Davisson <114700056+Tacoda23@users.noreply.github.com>
2023-02-16 17:04:23 -0600

    Update TB_Alerts.py
    won't get an index error when there's no current alerts

--- TB_Alerts.py
+++ TB_Alerts.py
@@ -18,6 +18,21 @@ state = "" # name of state
 API_key = "" # Visual Crossing Weather API Key
 unitGroup = "" # Units for measurement (us, metric, uk, base)
 
+client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
+auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
+#Check if already tweeted
+
+userlookup = client.get_user(username='OUWEATHERBOT')
+
+userid = (userlookup.data.id)
+
+tweets = client.get_users_tweets(id=userid, tweet_fields=['text'])
+
+test = str(tweets)
+
+
 def get_observation(API_key, city, state, unitGroup):
     
     url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}%20{state}?unitGroup={unitGroup}&key={API_key}&contentType=json'
@@ -28,48 +43,51 @@ def get_observation(API_key, city, state, unitGroup):
     
    # pull weather alert
     
-    event1 = response['alerts'][0]['event'] #pulls title
-    global eventupper1 
-    eventupper1 = (event1.upper()) #caps event
+    alerts = response['alerts']
 
-    global headline1
-    headline1 = response['alerts'][0]['headline'] #pulls description
-
-    global link1
-    link1 = response['alerts'][0]['link'] #pulls link
-
-    type_tiny = pyshorteners.Shortener() #shortens link
-    global shortlink1
-    shortlink1 = type_tiny.tinyurl.short(link1)
     
+    try:
+        event1 = response['alerts'][0]['event'] #pulls title
+	    
+        global eventupper1
+        eventupper1 = (event1.upper()) #caps event
 
-get_observation(API_key, city, state, unitGroup)
+        global headline1
+        headline1 = response['alerts'][0]['headline'] #pulls description
 
+        global link1
+        link1 = response['alerts'][0]['link'] #pulls link
 
+        type_tiny = pyshorteners.Shortener() #shortens link
+        global shortlink1
+        shortlink1 = type_tiny.tinyurl.short(link1)
 
-#print (eventupper1)
-#print (headline1)
-#print (shortlink1)
-
-alert_body = f"""
-{eventupper1}: {headline1}.
+        global alert_body
+        alert_body = f"""
+{eventupper1}: {headline1}. 
 Read more: {shortlink1}
-"""
+        """
+            
+        print (alert_body)
+        #print (test)
+        #print (headline1)
 
+        if headline1 in test:
+            print ('already tweeted')
+        else:
+            print ('just tweeted')
+            client.create_tweet(text=alert_body)
+            
 
-#print (alert_body)
+       
 
-client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
+    except:
+        event1 = "No current weather alerts"
+        print (event1)
 
-substring = 'http'
-
-#unnessary but helps prevent false posts, will get "index out of range" if no alerts
-if substring in link1: 
-    client.create_tweet(text=alert_body) #posts the tweet
-else:
-    print('No Alerts')
+    
+   
+get_observation(API_key, city, state, unitGroup)
 
 
 
